220321a8112ff9b69dac0b67cd0879ecc427da4e
Emahn Novid <emahn.novid@gmail.com>
2019-12-29 14:44:12 -0800

    Update string solutions

--- epi_judge_python/string_integer_interconversion.py
+++ epi_judge_python/string_integer_interconversion.py
@@ -1,15 +1,47 @@
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
+from icecream import ic
 
 
 def int_to_string(x):
-    # TODO - you fill in here.
-    return ''
+    if x == 0:
+        return '0'
+    d = {0: '0', 1: '1', 2: '2', 3: '3', 4: '4',
+         5: '5', 6: '6', 7: '7', 8: '8', 9: '9'}
+    s = ''
+    neg = x < 0
+    if neg:
+        x = -x
+    while x > 0:
+        s = d[x % 10] + s
+        x //= 10
+    if neg:
+        s = '-' + s
+    return s
 
 
 def string_to_int(s):
-    # TODO - you fill in here.
-    return 0
+    d = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4,
+         '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+    x = 0
+    neg = False
+    if s[0] == '-':
+        neg = True
+        s = s[1:]
+
+    for letter in s:
+        x = x * 10 + d[letter]
+
+    if neg:
+        x = -x
+    return x
+
+import functools
+def string_to_int(s):
+    return functools.reduce(lambda running_sum, c:
+                            running_sum * 10 + int(c), 
+                            s[s[0] == '-':], 0) * \
+                            (-1 if s[0] == '-' else 1) # Add sign
 
 
 def wrapper(x, s):
