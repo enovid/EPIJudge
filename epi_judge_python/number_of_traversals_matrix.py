88455ccae99c991413edddf663487cfef35666f1
Emahn Novid <emahn.novid@gmail.com>
2019-12-31 01:00:22 -0800

    Update solution

--- epi_judge_python/number_of_traversals_matrix.py
+++ epi_judge_python/number_of_traversals_matrix.py
@@ -2,8 +2,21 @@ from test_framework import generic_test
 
 
 def number_of_ways(n, m):
-    # TODO - you fill in here.
-    return 0
+    def pr(g):
+        for i, row in enumerate(g):
+            print(''.join(f'{s:^2}' for s in row))
+        print()
+
+    dp = [[0] * m for _ in range(n)] 
+    dp[0][0] = 1
+    # pr(dp)
+    for i in range(n):
+        for j in range(m):
+            for di, dj in [(-1,0),(0,-1)]:
+                row, col = i+di, j+dj
+                if 0 <= row < n and 0 <= col < m:
+                    dp[i][j] += dp[row][col]
+    return dp[-1][-1]
 
 
 if __name__ == '__main__':
