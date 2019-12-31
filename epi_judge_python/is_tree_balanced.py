910eea22a6f9c8218433eef67b4714887b93c047
Emahn Novid <emahn.novid@gmail.com>
2019-12-30 19:04:52 -0800

    Update solutions

--- epi_judge_python/is_tree_balanced.py
+++ epi_judge_python/is_tree_balanced.py
@@ -1,12 +1,52 @@
 from test_framework import generic_test
-
+from test_framework import test_utils_deserialization
+from binary_tree_with_parent_prototype import BinaryTreeNode
+from collections import deque, namedtuple
 
 def is_balanced_binary_tree(tree):
-    # TODO - you fill in here.
+    if not tree: return True
+    def height(root):
+        return 1 + max(height(root.left), height(root.right)) if root else 0
+
+    q = deque([tree])
+    while q:
+        t = q.popleft()
+        if abs(height(t.left) - height(t.right)) < 2:
+            if t.left:
+                q.append(t.left)
+            if t.right:
+                q.append(t.right)
+        else:
+            return False
     return True
 
+def is_balanced_binary_tree(tree):
+    BalancedHeightStatus = namedtuple('BalancedHeightStatus', ['is_balanced', 'height'])
+
+    def check_balance(t):
+        if not t:
+            return BalancedHeightStatus(True, -1)
+
+        left_status = check_balance(t.left)
+        if not left_status.is_balanced:
+            return BalancedHeightStatus(False, 0)
+        right_status = check_balance(t.right)
+        if not right_status.is_balanced:
+            return BalancedHeightStatus(False, 0)
+
+        balanced = abs(left_status.height - right_status.height) <= 1
+        if not balanced:
+            return BalancedHeightStatus(False, 0)
+        return BalancedHeightStatus(True, 1 + max(left_status.height, right_status.height))
+
+    return check_balance(tree).is_balanced
 
 if __name__ == '__main__':
+    # s = ["-4", "-5", "5", "-10", "-2", "6", "11", "null", "-9", "-2", "12"]
+    # t = test_utils_deserialization.build_binary_tree(s, lambda x: str(x), BinaryTreeNode)
+    # result = is_balanced_binary_tree(t)
+    # print(result)
+
     exit(
         generic_test.generic_test_main("is_tree_balanced.py",
                                        'is_tree_balanced.tsv',
--- epi_judge_python/number_of_score_combinations.py
+++ epi_judge_python/number_of_score_combinations.py
@@ -1,10 +1,57 @@
 from test_framework import generic_test
-
+from collections import defaultdict
+from icecream import ic
 
 def num_combinations_for_final_score(final_score, individual_play_scores):
-    # TODO - you fill in here.
-    return 0
+    def pr(g):
+        align_width = len(str(individual_play_scores))
+        scores = ''.join([' ' for _ in range(align_width + 3)]) + \
+                 ''.join(f'{i%10:^2}' for i in range(final_score + 1))
+        print(scores)
+        print(''.join('-' for _ in range(len(scores))))
+        for i, row in enumerate(g):
+            fmt_row = f'{str(individual_play_scores[:i+1]):>{align_width}} | ' +\
+                      ''.join(f'{s:^2}' for s in row)
+            print(fmt_row)
+        print()
+    dp = [[0] * (final_score + 1) for _ in range(len(individual_play_scores))]
 
+    for j in range(final_score+1):
+        if j % individual_play_scores[0] == 0:
+            dp[0][j] = 1
+
+    for i in range(1, len(dp)):
+        for j in range(final_score+1):
+            dp[i][j] = dp[i-1][j]
+            if j >= individual_play_scores[i]: 
+                dp[i][j] += dp[i][j-individual_play_scores[i]]
+
+    return dp[-1][-1]
+
+def num_combinations_for_final_score(final_score, individual_play_scores):
+    def pr(g):
+        align_width = len(str(individual_play_scores))
+        scores = ''.join([' ' for _ in range(align_width + 3)]) + \
+                 ''.join(f'{i%10:^2}' for i in range(final_score + 1))
+        print(scores)
+        print(''.join('-' for _ in range(len(scores))))
+        for i, row in enumerate(g):
+            fmt_row = f'{str(individual_play_scores[:i+1]):>{align_width}} | ' +\
+                      ''.join(f'{s:^2}' for s in row)
+            print(fmt_row)
+        print()
+    num_combinations_for_score = [[1] + [0] * final_score 
+                                  for _ in range(len(individual_play_scores))]  
+
+    for i in range(len(individual_play_scores)):
+        for j in range(1, final_score + 1):
+            with_this_play = (num_combinations_for_score[i][j-individual_play_scores[i]]
+                              if j >= individual_play_scores[i] else 0)
+            without_this_play = (num_combinations_for_score[i-1][j]
+                                 if i-1 >= 0 else 0)
+            num_combinations_for_score[i][j] = with_this_play + without_this_play
+
+    return num_combinations_for_score[-1][-1]
 
 if __name__ == '__main__':
     exit(
